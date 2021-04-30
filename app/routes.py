432a16e8f9dea5b064f34d305714411429ef5dfd
Glenda <chicag9313@gmail.com>
2021-04-29 22:49:59 -0700

    migrations

--- app/routes.py
+++ app/routes.py
@@ -17,4 +17,15 @@ def hello_world_json():
         "hobbies": ["hiking", "speding time with family", "handing out with friends"]
     }
 
+@hello_world_bp.route("/broken-endpoint-with-broken-server-code")
+def broken_endpoint():
+    response_body = {
+        "name": "Ada Lovelace",
+        "message": "Hello!",
+        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
+    }
+    new_hobby = "Surfing"
+    response_body["hobbies"].append(new_hobby)
+    return response_body
+
 
